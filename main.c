5bc26822ac5511c1273130be7c351ad12239e7e8
Daniyal Marofi <daniyaleight8@gmail.com>
2020-05-07 16:46:57 +0430

    buggy getArgument and signup

--- main.c
+++ main.c
@@ -1,7 +1,81 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct user
+{
+    char *username;
+    char *password;
+    char *userType;
+    int deposit;
+};
+
+char *getArgument()
+{
+    int size = 1;
+    char *str;
+    int ch;
+    size_t len = 0;
+    str = (char *)realloc(NULL, sizeof(char));
+    if (!str)
+        return str;
+    while (EOF != (ch = getchar()) && ch != ' ')
+    {
+        str[len++] = ch;
+        if (len == size)
+        {
+            str = (char *)realloc(str, sizeof(char) * (size += 16));
+            if (!str)
+                return str;
+        }
+    }
+    str[len++] = '\0';
+
+    return (char *)realloc(str, sizeof(char) * len);
+}
 
 int main()
 {
-    printf("Hello World!");
+    int numberOfUsers = 0;
+    struct user *users = NULL;
+    char command[20];
+
+    while (1)
+    {
+        printf("\nEnter your Command:");
+        int y = scanf("%s", command);
+        if (!strcmp(command, "signup"))
+        {
+            printf("you are trying to signup...");
+            getchar();
+            char *username = getArgument();
+            int usernameExists = 0;
+            int i = 0;
+            while (i = numberOfUsers && numberOfUsers > 0)
+            {
+                if (!strcmp(users[i].username, username))
+                {
+                    usernameExists = 1;
+                    printf("this username is taken. Please try another.");
+                    // clear Buffer
+                    while (getchar() != '\n')
+                        ;
+                    free(username);
+                    break;
+                }
+            }
+            if (!usernameExists)
+            {
+                numberOfUsers++;
+                users=(struct user*)realloc(users,numberOfUsers*sizeof(struct user));
+                users[numberOfUsers-1].username=username;
+                users[numberOfUsers-1].password=getArgument();
+                users[numberOfUsers-1].deposit=0;
+                users[numberOfUsers-1].userType=getArgument();
+                continue;
+            }
+        }
+    }
+
     return 0;
 }
