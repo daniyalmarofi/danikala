abd80bf666cb75b8a489ef3b135934850d45ba41
Daniyal Marofi <daniyaleight8@gmail.com>
2020-05-09 01:06:33 +0430

    seperated view

--- main.c
+++ main.c
@@ -106,7 +106,7 @@ void doSignup(char *input, int *numberOfUsers, struct user **users)
     // search for the username and usertype for not existing
     int usernameExists = 0;
     int i = 0;
-    while (i < *numberOfUsers && *numberOfUsers > 0)
+    while (i<*numberOfUsers && * numberOfUsers> 0)
     {
         if (!strcmp((*users)[i].username, username) && !strcmp((*users)[i].userType, userType))
         {
@@ -221,6 +221,64 @@ void doLogout(char *input, int *loggedinUserId)
     free(input);
 }
 
+void doView(char *input, struct user *users, int loggedinUserId, struct buyerCart *buyerCart, int buyerCartCount, struct good *goods, int numberOfGoods)
+{
+
+    if (strtok(NULL, " ") != NULL)
+    {
+        printf("too much input arguments!");
+        free(input);
+        return;
+    }
+
+    printf("Displaying user Information:\n");
+    printf("username: %s\t", users[loggedinUserId].username);
+    printf("userType: %s\t", users[loggedinUserId].userType);
+    printf("Deposit: %d\n", users[loggedinUserId].deposit);
+
+    if (!strcmp(users[loggedinUserId].userType, "buyer"))
+    {
+        int thisUserBasket = 0;
+        for (int i = 0; i < buyerCartCount; i++)
+        {
+            if (buyerCart[i].buyerId == loggedinUserId)
+            {
+                thisUserBasket++;
+                printf("----\n");
+                printf("Good Name:\t\t%s\n", goods[buyerCart[i].goodId].goodName);
+                printf("Good Price:\t\t%d\n", goods[buyerCart[i].goodId].goodPrice);
+                printf("Bought Count:\t\t%d\n", buyerCart[i].boughtCount);
+                printf("Seller Username:\t%s\n", users[goods[buyerCart[i].goodId].sellerId].username);
+            }
+        }
+        if (thisUserBasket == 0)
+        {
+            printf("you have no purchases!");
+        }
+    }
+    else if (!strcmp(users[loggedinUserId].userType, "seller"))
+    {
+        int thisUserGoods = 0;
+        for (int i = 0; i < numberOfGoods; i++)
+        {
+            if (goods[i].sellerId == loggedinUserId)
+            {
+                thisUserGoods++;
+                printf("----\n");
+                printf("Good Name:\t\t%s\n", goods[i].goodName);
+                printf("Good Price:\t\t%d\n", goods[i].goodPrice);
+                printf("Good Count:\t\t%d\n", goods[i].goodCount);
+            }
+        }
+        if (thisUserGoods == 0)
+        {
+            printf("you have no goods!");
+        }
+    }
+
+    free(input);
+}
+
 int main()
 {
 
@@ -256,59 +314,7 @@ int main()
         }
         else if (!strcmp(command, "view") && loggedinUserId != -1)
         {
-            if (strtok(NULL, " ") != NULL)
-            {
-                printf("too much input arguments!");
-                free(input);
-                continue;
-            }
-
-            printf("Displaying user Information:\n");
-            printf("username: %s\t", users[loggedinUserId].username);
-            printf("userType: %s\t", users[loggedinUserId].userType);
-            printf("Deposit: %d\n", users[loggedinUserId].deposit);
-
-            if (!strcmp(users[loggedinUserId].userType, "buyer"))
-            {
-                int thisUserBasket = 0;
-                for (int i = 0; i < buyerCartCount; i++)
-                {
-                    if (buyerCart[i].buyerId == loggedinUserId)
-                    {
-                        thisUserBasket++;
-                        printf("----\n");
-                        printf("Good Name:\t\t%s\n", goods[buyerCart[i].goodId].goodName);
-                        printf("Good Price:\t\t%d\n", goods[buyerCart[i].goodId].goodPrice);
-                        printf("Bought Count:\t\t%d\n", buyerCart[i].boughtCount);
-                        printf("Seller Username:\t%s\n", users[goods[buyerCart[i].goodId].sellerId].username);
-                    }
-                }
-                if (thisUserBasket == 0)
-                {
-                    printf("you have no purchases!");
-                }
-            }
-            else if (!strcmp(users[loggedinUserId].userType, "seller"))
-            {
-                int thisUserGoods = 0;
-                for (int i = 0; i < numberOfGoods; i++)
-                {
-                    if (goods[i].sellerId == loggedinUserId)
-                    {
-                        thisUserGoods++;
-                        printf("----\n");
-                        printf("Good Name:\t\t%s\n", goods[i].goodName);
-                        printf("Good Price:\t\t%d\n", goods[i].goodPrice);
-                        printf("Good Count:\t\t%d\n", goods[i].goodCount);
-                    }
-                }
-                if (thisUserGoods == 0)
-                {
-                    printf("you have no goods!");
-                }
-            }
-
-            free(input);
+            doView(input, users, loggedinUserId, buyerCart, buyerCartCount, goods, numberOfGoods);
         }
         else if (!strcmp(command, "deposit") && loggedinUserId != -1 && !strcmp(users[loggedinUserId].userType, "buyer"))
         {
