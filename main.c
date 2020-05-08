49d046de22544d7cd994bcfcff5336fe8d8542f6
Daniyal Marofi <daniyaleight8@gmail.com>
2020-05-09 01:23:47 +0430

    seperated add goods

--- main.c
+++ main.c
@@ -279,6 +279,131 @@ void doView(char *input, struct user *users, int loggedinUserId, struct buyerCar
     free(input);
 }
 
+void doDeposit(char *input, struct user **users, int loggedinUserId)
+{
+    // get the deposit value and convert it to integer and add it to user deposit
+    char *deposit = strtok(NULL, " ");
+    int depositvalue = 0;
+    if (checkInput(deposit))
+        return;
+
+    if (strtok(NULL, " ") != NULL)
+    {
+        printf("too much input arguments!");
+        free(input);
+        return;
+    }
+
+    if ((depositvalue = atoi(deposit)) > 0)
+    {
+        (*users)[loggedinUserId].deposit += depositvalue;
+        printf("%d successfuly added to your deposit.\nNow your deposit is %d.", depositvalue, (*users)[loggedinUserId].deposit);
+    }
+    else
+        printf("Wrong input! Try again!");
+
+    free(input);
+}
+
+void doAddGoods(char *input, struct user *users, int loggedinUserId, struct good **goods, int *numberOfGoods)
+{
+    // getting and checking goodName and goodPrice and goodCount from input
+    char *sellerUsername = strtok(NULL, " ");
+    if (checkInput(sellerUsername))
+    {
+        free(input);
+        return;
+    }
+    char *goodName = strtok(NULL, " ");
+    if (checkInput(goodName))
+    {
+        free(input);
+        return;
+    }
+    char *goodPrice = strtok(NULL, " ");
+    if (checkInput(goodPrice))
+    {
+        free(input);
+        return;
+    }
+    char *goodCount = strtok(NULL, " ");
+    if (checkInput(goodCount))
+    {
+        free(input);
+        return;
+    }
+
+    if (strtok(NULL, " ") != NULL)
+    {
+        printf("too much input arguments!");
+        free(input);
+        return;
+    }
+
+    int goodPriceValue = atoi(goodPrice);
+    int goodCountValue = atoi(goodCount);
+    if (goodPriceValue <= 0 || goodCountValue <= 0)
+    {
+        printf("Wrong Input! Try again!");
+        free(input);
+        return;
+    }
+
+    if (strcmp(sellerUsername, users[loggedinUserId].username))
+    {
+        printf("you can not add or chnge count of this good!");
+        free(input);
+        return;
+    }
+
+    // search for the goodName for not existing
+    // if good exists I'd assign the id of that good to goodExists Variable
+    int goodExists = -1;
+    int i = 0;
+    while (i < *numberOfGoods && *numberOfGoods > 0)
+    {
+        if (!strcmp((*goods)[i].goodName, goodName))
+        {
+            goodExists = i;
+            break;
+        }
+        i++;
+    }
+
+    // if new Good does not exists add it to goods
+    if (goodExists = -1)
+    {
+        char *newGoodname = (char *)malloc(sizeof(char) * strlen(goodName));
+        checkMalloc(newGoodname);
+        strcpy(newGoodname, goodName);
+
+        free(input);
+
+        (*numberOfGoods)++;
+        *goods = (struct good *)realloc(*goods, *numberOfGoods * sizeof(struct good));
+        checkMalloc(*goods);
+
+        (*goods)[*numberOfGoods - 1].sellerId = loggedinUserId;
+        (*goods)[*numberOfGoods - 1].goodName = newGoodname;
+        (*goods)[*numberOfGoods - 1].goodPrice = goodPriceValue;
+        (*goods)[*numberOfGoods - 1].goodCount = goodCountValue;
+
+        printf("Good Successfully added.");
+    }
+    else
+    {
+        if (!strcmp(users[(*goods)[goodExists].sellerId].username, users[loggedinUserId].username))
+        {
+            (*goods)[goodExists].goodCount = goodCountValue;
+            printf("goodCount changed!");
+        }
+        else
+            printf("you are not the seller of this good!");
+
+        free(input);
+    }
+}
+
 int main()
 {
 
@@ -318,123 +443,11 @@ int main()
         }
         else if (!strcmp(command, "deposit") && loggedinUserId != -1 && !strcmp(users[loggedinUserId].userType, "buyer"))
         {
-            if (strtok(NULL, " ") != NULL)
-            {
-                printf("too much input arguments!");
-                free(input);
-                continue;
-            }
-            // get the deposit value and convert it to integer and add it to user deposit
-            char *deposit = strtok(NULL, " ");
-            int depositvalue = 0;
-            if (checkInput(deposit))
-                continue;
-            if ((depositvalue = atoi(deposit)) > 0)
-            {
-                users[loggedinUserId].deposit += depositvalue;
-                printf("%d successfuly added to your deposit.\nNow your deposit is %d.", depositvalue, users[loggedinUserId].deposit);
-            }
-            else
-                printf("Wrong input! Try again!");
-
-            free(input);
+            doDeposit(input, &users, loggedinUserId);
         }
         else if (!strcmp(command, "add_goods") && loggedinUserId != -1 && !strcmp(users[loggedinUserId].userType, "seller"))
         {
-            if (strtok(NULL, " ") != NULL)
-            {
-                printf("too much input arguments!");
-                free(input);
-                continue;
-            }
-
-            // getting and checking goodName and goodPrice and goodCount from input
-            char *sellerUsername = strtok(NULL, " ");
-            if (checkInput(sellerUsername))
-            {
-                free(input);
-                continue;
-            }
-            char *goodName = strtok(NULL, " ");
-            if (checkInput(goodName))
-            {
-                free(input);
-                continue;
-            }
-            char *goodPrice = strtok(NULL, " ");
-            if (checkInput(goodPrice))
-            {
-                free(input);
-                continue;
-            }
-            char *goodCount = strtok(NULL, " ");
-            if (checkInput(goodCount))
-            {
-                free(input);
-                continue;
-            }
-            int goodPriceValue = atoi(goodPrice);
-            int goodCountValue = atoi(goodCount);
-            if (goodPriceValue <= 0 || goodCountValue <= 0)
-            {
-                printf("Wrong Input! Try again!");
-                free(input);
-                continue;
-            }
-
-            if (strcmp(sellerUsername, users[loggedinUserId].username))
-            {
-                printf("you can not add or chnge count of this good!");
-                free(input);
-                continue;
-            }
-
-            // search for the goodName for not existing
-            // if good exists I'd assign the id of that good to goodExists Variable
-            int goodExists = -1;
-            int i = 0;
-            while (i < numberOfGoods && numberOfGoods > 0)
-            {
-                if (!strcmp(goods[i].goodName, goodName))
-                {
-                    goodExists = i;
-                    break;
-                }
-                i++;
-            }
-
-            // if new Good does not exists add it to goods
-            if (goodExists = -1)
-            {
-                char *newGoodname = (char *)malloc(sizeof(char) * strlen(goodName));
-                checkMalloc(newGoodname);
-                strcpy(newGoodname, goodName);
-
-                free(input);
-
-                numberOfGoods++;
-                goods = (struct good *)realloc(goods, numberOfGoods * sizeof(struct good));
-                checkMalloc(goods);
-
-                goods[numberOfGoods - 1].sellerId = loggedinUserId;
-                goods[numberOfGoods - 1].goodName = newGoodname;
-                goods[numberOfGoods - 1].goodPrice = goodPriceValue;
-                goods[numberOfGoods - 1].goodCount = goodCountValue;
-
-                printf("Good Successfully added.");
-            }
-            else
-            {
-                if (!strcmp(users[goods[goodExists].sellerId].username, users[loggedinUserId].username))
-                {
-                    goods[goodExists].goodCount = goodCountValue;
-                    printf("goodCount changed!");
-                }
-                else
-                    printf("you are not the seller of this good!");
-
-                free(input);
-            }
+            doAddGoods(input, users, loggedinUserId, &goods, &numberOfGoods);
         }
         else if (!strcmp(command, "show_goods") && loggedinUserId != -1)
         {
